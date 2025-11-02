fb3795b73cc4b1dd9a4054a5937aaf96c4134f56
Igor Shabunin <shabunin181104@gmail.com>
2025-11-02 16:49:47 +0500

    Update app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,7 @@
 import GameField from '@/components/game-field/game-field.tsx'
 import Menu from '@/components/menu/menu.tsx'
-import Overlay from '@/components/overlay/overlay.tsx'
-import { useState } from 'react'
-export default function App() {
-  const [isOpen, setIsOpen] = useState(false)
 
+export default function App() {
   return (
     <>
       <Menu />
