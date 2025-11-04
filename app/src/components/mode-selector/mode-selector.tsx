2c4e6d4bc5143830f096ac96a1ac7cc50572a103
Igor Shabunin <shabunin181104@gmail.com>
2025-11-05 01:28:07 +0500

    Update mode-selector.tsx

--- app/src/components/mode-selector/mode-selector.tsx
+++ app/src/components/mode-selector/mode-selector.tsx
@@ -8,13 +8,13 @@ export default function ModeSelector() {
     <>
       <div className={styles.info}>Choose game mode</div>
       <div className={styles.wrapper}>
-        <button className={styles.card} onClick={() => changeMode('1vs1')}>
-          <div className={styles.cardInner}>
-            <img src="/user-svgrepo-com.svg" alt="user" />
-            vs
-            <img src="/user-svgrepo-com.svg" alt="user" />
-          </div>
-        </button>
+        {/*<button className={styles.card} onClick={() => changeMode('1vs1')}>*/}
+        {/*  <div className={styles.cardInner}>*/}
+        {/*    <img src="/user-svgrepo-com.svg" alt="user" />*/}
+        {/*    vs*/}
+        {/*    <img src="/user-svgrepo-com.svg" alt="user" />*/}
+        {/*  </div>*/}
+        {/*</button>*/}
         <button className={styles.card} onClick={() => changeMode('bot')}>
           <div className={styles.cardInner}>
             <img src="/user-svgrepo-com.svg" alt="user" />
